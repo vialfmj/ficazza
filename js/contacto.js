0dd23f650a38ad872f7f755941b418e613c37d53
anto guarnieri <antosofiag@gmail.com>
2021-09-09 18:13:08 -0300

    avance con jquery

---
+++ js/contacto.js
@@ -0,0 +1,37 @@
+/* scripts de la seccion contacto */
+let comentarios=[];
+class Comentario {
+    constructor(nombre, email,mensaje){
+        this.nombre= nombre;
+        this.email= email;
+        this.mensaje= mensaje;  
+        
+    }
+
+}
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+function mostrarComentarios(){
+    let guardados= JSON.parse(localStorage.getItem("listaComentarios"));
+    let comentarios= [];
+    for (const objeto of guardados){
+            comentarios.push(new Comentario(objeto));
+    }
+    for (const comentario of comentarios){
+        console.log(comentarios);
+    }
+     
+}
+function obtenerComentario(){
+
+}
+$(document).ready(()=> {
+    $("#contacto-formulario").submit(function(){
+        let nombre= $('#contacto-nombre').val();
+        let email= $('#contacto-email').val();
+        let mensaje= $('#contacto-comentario').val();
+        console.log("hola");
+        comentarios.push(new Comentario(nombre, email, mensaje));
+        guardarLocal("listaComentarios", JSON.stringify(comentarios));
+        mostrarComentarios();
+    })
+})
